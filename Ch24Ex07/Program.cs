8ca8c37f9d5e5ec05c3a37975e8f8fdcf4a064ef
MindOverflow <toxaSmith@yahoo.com>
2017-03-24 23:46:46 +0300

    [change]  Решарпер рефайнмент. И убрал метод Dispose у тасков. Всё равно не несёт никакой нагрузки.

--- Ch24Ex07/Program.cs
+++ Ch24Ex07/Program.cs
@@ -25,14 +25,11 @@ namespace Ch24Ex07
         {
             WriteLine("Основной поток выполнения запущен.");
 
-            Task<bool> task01 = Task<bool>.Factory.StartNew(MyTask);
+            var task01 = Task<bool>.Factory.StartNew(MyTask);
             WriteLine($"Результат после выполнения задачи MyTask: {task01.Result}");
 
-            Task<int> task02 = Task<int>.Factory.StartNew(SumIt, 3);
+            var task02 = Task<int>.Factory.StartNew(SumIt, 3);
             WriteLine($"Результат после выполнения задачи SumIt {task02.Result}");
-
-            task01.Dispose();
-            task02.Dispose();
 
             WriteLine("Основной поток выполнения завершён.");
         }
