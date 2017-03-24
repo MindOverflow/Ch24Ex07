038000ca3d5f639af5375968bed72a1558bef250
MindOverflow <toxaSmith@yahoo.com>
2017-03-24 23:44:14 +0300

    [add] Initial commit. Изначальный вариант вариант.

---
+++ Ch24Ex07/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Threading.Tasks;
+using static System.Console;
+
+namespace Ch24Ex07
+{
+    internal static class Program
+    {
+        private static bool MyTask()
+        {
+            return true;
+        }
+
+        private static int SumIt(object v)
+        {
+            var x = (int) v;
+            var sum = 0;
+            for (; x > 0; x--)
+            {
+                sum = sum + x;
+            }
+            return sum;
+        }
+
+        private static void Main()
+        {
+            WriteLine("Основной поток выполнения запущен.");
+
+            Task<bool> task01 = Task<bool>.Factory.StartNew(MyTask);
+            WriteLine($"Результат после выполнения задачи MyTask: {task01.Result}");
+
+            Task<int> task02 = Task<int>.Factory.StartNew(SumIt, 3);
+            WriteLine($"Результат после выполнения задачи SumIt {task02.Result}");
+
+            task01.Dispose();
+            task02.Dispose();
+
+            WriteLine("Основной поток выполнения завершён.");
+        }
+    }
+}
